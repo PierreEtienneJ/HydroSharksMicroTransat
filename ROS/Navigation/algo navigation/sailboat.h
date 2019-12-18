639260de2a1c2040421628c4de7ac9c2e4737633
tgod310 <58785544+tgod310@users.noreply.github.com>
2019-12-18 17:03:13 +0100

    Update sailboat.h

--- ROS/Navigation/algo navigation/sailboat.h
+++ ROS/Navigation/algo navigation/sailboat.h
@@ -16,8 +16,8 @@ void suivideligne();
 
 
 double x,y,v,theta,omega; // state variables x,y : position ; v : vitesse ; theta : cap ; omega : variation de cap
-double beta, alphag; // parameters beta : coeff dérive alphag : coeff de portance du gouvernail deltag : angle du gouvernail
-double a,psi; // wind a : vitesse  psi : angle
+double beta, alphag; // parametres beta : coeff dérive ; alphag : coeff de portance du gouvernail ; deltag : angle du gouvernail
+double a,psi; // wind a : vitesse ; psi : angle
 double dt;  // pas
 //double fg;  //link variables  fg : force de l'eau sur le gouvernail
 
