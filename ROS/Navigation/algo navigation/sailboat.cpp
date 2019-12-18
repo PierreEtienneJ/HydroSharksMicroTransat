89cab37512591e8b047d193f5cedd99e70a68020
Yann <yann.darchen@gmail.com>
2019-12-18 18:06:34 +0100

    update 42.42.42

--- ROS/Navigation/algo navigation/sailboat.cpp
+++ ROS/Navigation/algo navigation/sailboat.cpp
@@ -58,59 +58,64 @@ Sailboat::Sailboat() /// Initialisation
     q=1;
 }
 
+/// Définition de la fonction sign ///
 double sign(double a)
-{if (a>0) return 1; else return -1;};
+{
+    if (a>0) return 1; else return -1;
+};
 
 void Sailboat::avancer()
 {
+    double xw_ap=a*cos(psi-theta)-v;
+    double yw_ap=a*sin(psi-theta);
+    double psi_ap=atan2(yw_ap,xw_ap);   //Apparent wind
+    double a_ap=sqrt(xw_ap*xw_ap+yw_ap*yw_ap);
+    gamma=cos(psi_ap)+cos(deltavmax);
 
-double xw_ap=a*cos(psi-theta)-v;
-double yw_ap=a*sin(psi-theta);
-double psi_ap=atan2(yw_ap,xw_ap);   //Apparent wind
-double a_ap=sqrt(xw_ap*xw_ap+yw_ap*yw_ap);
-gamma=cos(psi_ap)+cos(deltavmax);
-if (gamma<0) {deltav=M_PI+psi_ap;} //voile en drapeau
-else  if (sin(-psi_ap)>0) deltav=deltavmax;   else deltav=-deltavmax;
-fg = alphag*v*sin(deltag);
-fv = alphav*a_ap*sin(deltav-psi_ap);
+    if (gamma<0) {deltav=M_PI+psi_ap;} //voile en drapeau
+    else  if (sin(-psi_ap)>0) deltav=deltavmax;   else deltav=-deltavmax;
+    fg = alphag*v*sin(deltag);
+    fv = alphav*a_ap*sin(deltav-psi_ap);
 
-x += (v*cos(theta)+beta*a*cos(psi))*dt;
-y += (v*sin(theta)+beta*a*sin(psi))*dt;
-theta += omega*dt;
-
-
-omega += (1/Jz)*((l-rv*cos(deltav))*fv-rg*cos(deltag)*fg-alphatheta*omega*v)*dt;
-v += (1/m)*(sin(deltav)*fv-sin(deltag)*fg-alphaf*v*v)*dt;
-phiPoint += (-phiPoint+fv*hv*cos(deltav)*cos(phi)/Jx - 10000*9.81*sin(phi)/Jx)*dt ;
-phi += phiPoint * dt;
+    x += (v*cos(theta)+beta*a*cos(psi))*dt;
+    y += (v*sin(theta)+beta*a*sin(psi))*dt;
+    theta += omega*dt;
 
 
+    omega += (1/Jz)*((l-rv*cos(deltav))*fv-rg*cos(deltag)*fg-alphatheta*omega*v)*dt;
+    v += (1/m)*(sin(deltav)*fv-sin(deltag)*fg-alphaf*v*v)*dt;
+    phiPoint += (-phiPoint+fv*hv*cos(deltav)*cos(phi)/Jx - 10000*9.81*sin(phi)/Jx)*dt ;
+    phi += phiPoint * dt;
 }
 
 void Sailboat::suivideligne()
 {
-double r=50; ///ecart a la ligne accepté///
-double zeta=M_PI/4;
-e=((bx-ax)*(y-ay)-(x-ax)*(by-ay))/hypot(ax-bx,ay-by); ///calcul de l'écart a la ligne///
-if (fabs(e)>r) q=0; ///si l'écart est trop important///
-double phi=atan2(by-ay,bx-ax); ///angle de la ligne///
-double thetabar=phi-0.5*atan(e/r);///cap tenable///
-if ((q==0)&((cos(psi-thetabar)+cos(zeta)<0)|((fabs(e)<r)&(cos(psi-phi)+cos(zeta)<0))))
+    double r=50; ///ecart a la ligne accepté///
+    double zeta=M_PI/4;
+    e=((bx-ax)*(y-ay)-(x-ax)*(by-ay))/hypot(ax-bx,ay-by); ///calcul de l'écart a la ligne///
+    if (fabs(e)>r) q=0; ///si l'écart est trop important///
+    double phi=atan2(by-ay,bx-ax); ///angle de la ligne///
+    double thetabar=phi-0.5*atan(e/r);///cap tenable///
+
+    if ((q==0)&((cos(psi-thetabar)+cos(zeta)<0)|((fabs(e)<r)&(cos(psi-phi)+cos(zeta)<0))))
     {
-    q=sign(e);
+        q=sign(e);
     }
-theta += omega*dt;
-omega += (1/Jz)*((l-rv*cos(deltav))*fv-rg*cos(deltag)*fg-alphatheta*omega*v)*dt;
-v += (1/m)*(sin(deltav)*fv-sin(deltag)*fg-alphaf*v*v)*dt;
-phiPoint += (-phiPoint+fv*hv*cos(deltav)*cos(phi)/Jx - 10000*9.81*sin(phi)/Jx)*dt ;
-phi += phiPoint * dt;
-if (q!=0)
-  {
-  thetabar=M_PI+psi-zeta*q;
-  }
-double dtheta=theta-thetabar;
-deltag=(1/M_PI)*(atan(tan(0.5*dtheta)));
-deltavmax=0.5*M_PI*(0.5*(cos(psi-thetabar)+1));
+
+    theta += omega*dt;
+    omega += (1/Jz)*((l-rv*cos(deltav))*fv-rg*cos(deltag)*fg-alphatheta*omega*v)*dt;
+    v += (1/m)*(sin(deltav)*fv-sin(deltag)*fg-alphaf*v*v)*dt;
+    phiPoint += (-phiPoint+fv*hv*cos(deltav)*cos(phi)/Jx - 10000*9.81*sin(phi)/Jx)*dt ;
+    phi += phiPoint * dt;
+
+    if (q!=0)
+    {
+        thetabar=M_PI+psi-zeta*q;
+    }
+
+    double dtheta=theta-thetabar;
+    deltag=(1/M_PI)*(atan(tan(0.5*dtheta)));
+    deltavmax=0.5*M_PI*(0.5*(cos(psi-thetabar)+1));
 
 }
 
